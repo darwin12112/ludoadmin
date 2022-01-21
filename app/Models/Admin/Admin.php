5182e31706cccba1f52d68ed1aeb93c8f0fa47dc
darwin12112 <darwinkk000@gmail.com>
2022-01-21 20:35:23 +0530

    ludoindian

---
+++ app/Models/Admin/Admin.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+
+class Admin extends Eloquent
+{
+    use HasFactory;
+
+    protected $fillable = [
+            "name",
+            "username",
+            "email",
+            "role",
+            "password",
+            "bio",
+            "birthdate",
+            "website",
+            "phone",
+            "country",
+            "company",
+            "profile_img",
+            "work",
+            "publish_year",
+            "facebook",
+            "instagram",
+            "twitter",
+            "linkedin",
+            "youtube",
+            "whatsapp",
+    ];
+}
